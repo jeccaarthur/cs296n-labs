df9dab782a0f0f34c527c9c9db8c64563ccf1fa8
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-07 19:05:58 -0800

    Added validation

--- JeccaArthurSite/JeccaArthurSite/Models/Message.cs
+++ JeccaArthurSite/JeccaArthurSite/Models/Message.cs
@@ -1,21 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Winterfell.Models
 {
     public class Message
     {
-        public Message()
-        {
-        }
-
         public int MessageID { get; set; }
 
         public User Sender { get; set; }
 
         public User Recipient { get; set; }
 
+        [Required]
         public string Subject { get; set; }
 
+        [Required]
         public string Body { get; set; }
 
         public DateTime Date { get; set; }
