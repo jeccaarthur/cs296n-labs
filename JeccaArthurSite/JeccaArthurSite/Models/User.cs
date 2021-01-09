3b5d66b0b183fe711efac1193a6f3a0bf8e12c0c
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-08 20:33:51 -0800

    Removed unnecessary property

--- JeccaArthurSite/JeccaArthurSite/Models/User.cs
+++ JeccaArthurSite/JeccaArthurSite/Models/User.cs
@@ -11,12 +11,8 @@ namespace Winterfell.Models
         public int UserID { get; set; }
 
         [Required]
-        [StringLength(20, MinimumLength = 2)]
-        public string Username { get; set; }
-
-        [Required]
         [StringLength(60, MinimumLength = 1)]
-        public string Name { get; set; }
+        public string Username { get; set; }
 
         // public string Email { get; set; }
     }
