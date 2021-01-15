ee0f0090405931eaa39f23b743a2062fe010b502
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-15 12:43:39 -0800

    updated to inherit from Identity, removed ID property, and renamed to AppUser

--- JeccaArthurSite/JeccaArthurSite/Models/User.cs
+++ JeccaArthurSite/JeccaArthurSite/Models/User.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Winterfell.Models
 {
-    public class User
+    public class AppUser : IdentityUser
     {
-        public int UserID { get; set; }
-
         [Required(ErrorMessage = "User's name is required")]
         [StringLength(60, MinimumLength = 1)]
         [RegularExpression(@"^[a-zA-Z]{1,60}$", ErrorMessage = "User's name must be alphabetic characters only")]
         public string Name { get; set; }
-
-        // public string Email { get; set; }
     }
 }
