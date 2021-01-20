ea9593790b27bfcf25ef2883f70bc0db05e3c6cb
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-19 19:55:37 -0800

    added Register Post method

--- JeccaArthurSite/JeccaArthurSite/Controllers/AccountController.cs
+++ JeccaArthurSite/JeccaArthurSite/Controllers/AccountController.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Winterfell.Models;
 
 namespace Winterfell.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMngr, SignInManager<AppUser> signInMngr)
+        {
+            userManager = userMngr;
+            signInManager = signInMngr;
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.Username };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
         }
     }
 }
