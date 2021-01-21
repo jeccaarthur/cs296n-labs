83e50ff5e952e137b4102d11b9aa9829d0dc08a9
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-21 10:22:54 -0800

    added properties from textbook

--- JeccaArthurSite/JeccaArthurSite/Models/LoginVM.cs
+++ JeccaArthurSite/JeccaArthurSite/Models/LoginVM.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Winterfell.Models
 {
     public class LoginVM
     {
-        public LoginVM()
-        {
-        }
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
