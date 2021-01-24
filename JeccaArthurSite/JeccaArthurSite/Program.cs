048a7f65a9958921a85c04012c821a79cd40137c
Jecca Arthur <arthurj@my.lanecc.edu>
2021-01-23 17:15:27 -0800

    set ValidateScopes to false

--- JeccaArthurSite/JeccaArthurSite/Program.cs
+++ JeccaArthurSite/JeccaArthurSite/Program.cs
@@ -20,7 +20,8 @@ namespace Winterfell
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseDefaultServiceProvider(
+                        options => options.ValidateScopes = false);
                 });
     }
 }
