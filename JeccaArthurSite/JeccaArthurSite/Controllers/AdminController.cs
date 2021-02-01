b71edfdf4ac8996a8b039b9847e0333d069230d3
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-01 12:55:07 -0800

    added Index method

--- JeccaArthurSite/JeccaArthurSite/Controllers/AdminController.cs
+++ JeccaArthurSite/JeccaArthurSite/Controllers/AdminController.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Winterfell.Models;
+using System.Linq;
+using Winterfell.Repositories;
+
 namespace Winterfell.Controllers
 {
-    public class UserController
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
     {
-        public UserController()
+        private UserManager<AppUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public AdminController(UserManager<AppUser> userMngr, RoleManager<IdentityRole> roleMngr)
         {
+            userManager = userMngr;
+            roleManager = roleMngr;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<AppUser> users = new List<AppUser>();
+            foreach (AppUser user in userManager.Users)
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user);
+                users.Add(user);
+            }
+
+            AdminVM model = new AdminVM
+            {
+                Users = users,
+                Roles = roleManager.Roles
+            };
+
+            return View(model);
         }
     }
 }
