f652c2603334f677918b8d722fae8d99576b7152
Jecca Arthur <arthurj@my.lanecc.edu>
2021-02-15 20:41:07 -0800

    added code to resolve CORS and X-Frame-Options vulnerabilities

--- JeccaArthurSite/JeccaArthurSite/Startup.cs
+++ JeccaArthurSite/JeccaArthurSite/Startup.cs
@@ -4,10 +4,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Winterfell.Models;
 using System.Runtime.InteropServices;
 using Winterfell.Repositories;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
 
 namespace Winterfell
 {
@@ -20,12 +23,25 @@ namespace Winterfell
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // added for lab 6 - security checks
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("https://maxcdn.bootstrapcdn.com", "https://localhost:5001")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+
             services.AddControllersWithViews();
-            services.AddRazorPages()
-            .AddRazorRuntimeCompilation();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
 
             // add service for DbContext with SQLite - this is dependency injection
             // add if statement to support azure db
@@ -45,6 +61,7 @@ namespace Winterfell
             services.AddTransient<IMessages, MessagesRepository>(); // specify repository interface, then repository
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MessageContext context)
         {
@@ -62,6 +79,19 @@ namespace Winterfell
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            // enable CORS
+            app.UseCors();
+
+            // set x-frame options
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                await next();
+            });
+
+            // vulnerability: cookie without secure flag
+            app.UseCookiePolicy(new CookiePolicyOptions { HttpOnly = HttpOnlyPolicy.Always, Secure = CookieSecurePolicy.Always });
 
             app.UseAuthentication();
             app.UseAuthorization();
